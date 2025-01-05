720214311e277dd70c67d5a56efe9e5cdd9a5b9d
Bingusala <139640476+Bingusala@users.noreply.github.com>
2025-01-05 03:29:12 +0000

    Create Home component with basic layout and ScrollView structure.

--- mobileApp/components/Home.js
+++ mobileApp/components/Home.js
@@ -1,3 +1,17 @@
 import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, View, ScrollView } from 'react-native';
 import { PaperProvider, Text, Divider, Button } from 'react-native-paper';
+
+export default function Home() {
+    const navigation = useNavigation();
+    
+    return (
+        <PaperProvider>
+            <ScrollView contentContainerStyle={styles.scrollView}>
+                <View style={styles.container}>
+                    {/* Content goes here */}
+                </View>
+            </ScrollView>
+        </PaperProvider>
+    );
+}
