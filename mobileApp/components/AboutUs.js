ecd6f0ee59de4cb4e182618ed41f11670156c892
Bingusala <139640476+Bingusala@users.noreply.github.com>
2025-01-05 06:10:06 +0000

    Include image and styling for the university logo.

--- mobileApp/components/AboutUs.js
+++ mobileApp/components/AboutUs.js
@@ -6,8 +6,14 @@ import { PaperProvider, Text, Divider, Button } from 'react-native-paper';
 export default function AboutUs() {
     const navigation = useNavigation();
     return (
-
-        <PaperProvider>
-
-            <ScrollView contentContainerStyle={styles.scrollView}>
+<PaperProvider>
+<ScrollView contentContainerStyle={styles.scrollView}>
                 <View style={styles.container}>
+                   
+                    <Text variant="headlineLarge">History</Text>
+                    
+                    <Divider />
+                    <View style={styles.imagepad}>
+    <Image source={require('../assets/uovlogo.png')} style={styles.image} />
+</View>
+
