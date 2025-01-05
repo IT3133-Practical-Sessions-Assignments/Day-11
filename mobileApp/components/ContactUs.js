567fe9815d9f2a5f41159a55664c222861e85937
Bingusala <139640476+Bingusala@users.noreply.github.com>
2025-01-05 06:23:47 +0000

    Add header with title and divider for ContactUs.

--- mobileApp/components/ContactUs.js
+++ mobileApp/components/ContactUs.js
@@ -7,4 +7,18 @@ import { PaperProvider, Text, Divider, TextInput, Button } from 'react-native-pa
 export default function ContactUs() {
     const [name, setName] = useState();
     const navigation = useNavigation();
+    return (
+        <PaperProvider>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.keyboardAvoidingView}
+            >
+
+                <View style={styles.header}>
+                    <Text variant="headlineLarge">Contact Us</Text>
+                    <Divider />
+                </View>
+            </KeyboardAvoidingView>
+        </PaperProvider>
+
+    );
 }
